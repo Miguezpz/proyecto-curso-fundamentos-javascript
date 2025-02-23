32c3028a042f4d030d2e93caaa7e194b7100aa6e
Miguezpz <miguezpzmx@gmail.com>
2025-02-23 17:41:17 -0600

    I started coding 3rd website.

---
+++ apiRandomNumber/script.js
@@ -0,0 +1,5 @@
+//const API_URL = 'https://api-random-number-miguezpz.onrender.com/randomNumber/min/max'
+
+/* fetch ('https://api-random-number-miguezpz.onrender.com/randomNumber/1/5')
+    .then(res => res.json())
+    .then(data => console.log('Número Random:', data.randomNumber)) */
